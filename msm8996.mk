314cf227bedd74d7c5650579642410d6a295351f
albertoduqe <albertoduqe@gmail.com>
2018-12-19 09:59:50 +0100

    Remove xiaomiparts

--- msm8996.mk
+++ msm8996.mk
@@ -389,5 +389,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
+#PRODUCT_PACKAGES += \
+#    XiaomiParts
